58e9b9aff4227199d30b25a799455c1e6f4a8325
Hypkeh <144148068+Hypkeh@users.noreply.github.com>
2024-05-25 21:32:14 +0500

    Add files via upload

--- hwdjgo/ManyToManyProject/manytomany_app/models.py
+++ hwdjgo/ManyToManyProject/manytomany_app/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Student(models.Model):
     name =models.CharField(max_length=30)
     age = models.IntegerField()
+    courses = models.ForeignKey('Course', on_delete=models.CASCADE)
+
+class Course(models.Model):
+    name = models.CharField(max_length=30)
